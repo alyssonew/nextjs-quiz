bdd8e7d30bb09cf7fd07d195d0da49f251caab0f
Alysson <alyssonext@gmail.com>
2021-09-30 23:13:50 -0300

    correcting view

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,9 @@ import Quiz from '../components/Quiz'
 import QuestionModel from '../model/question'
 import { useRouter } from 'next/router'
 
-const BASE_URL= 'https://nextjs-quiz-ebon.vercel.app/api'
+const BASE_URL = 'http://localhost:3000/api'
+
+/* https://nextjs-quiz-ebon.vercel.app/api */
 
 export default function Home() {
 
@@ -13,13 +15,13 @@ export default function Home() {
   const [question, setQuestion] = useState<QuestionModel>()
   const [hits, setHits] = useState<number>(0)
 
-  async function getQuestionsIds(){
+  async function getQuestionsIds() {
     const resp = await fetch(`${BASE_URL}/quiz`)
     const ids = await resp.json()
     setIds(ids)
   }
 
-  async function loadQuestion(id: number){
+  async function loadQuestion(id: number) {
     const resp = await fetch(`${BASE_URL}/questions/${id}`)
     const json = await resp.json()
     const newQuestion = QuestionModel.fromObject(json)
@@ -28,30 +30,30 @@ export default function Home() {
 
   useEffect(() => {
     getQuestionsIds()
-  
+
   }, [])
 
   useEffect(() => {
 
-    ids.length > 0 &&loadQuestion(ids[0])
-  
+    ids.length > 0 && loadQuestion(ids[0])
+
   }, [ids])
 
-  function answeredQuestion(answeredQuestion: QuestionModel){
+  function answeredQuestion(answeredQuestion: QuestionModel) {
     setQuestion(answeredQuestion)
     const isRight = answeredQuestion.rightAnswer
     setHits(hits + (isRight ? 1 : 0))
-   
+
   }
 
-  function idNextQuestion(){
+  function idNextQuestion() {
     if (question) {
-      const nextIndex =  ids.indexOf(question.id) + 1
+      const nextIndex = ids.indexOf(question.id) + 1
       return ids[nextIndex]
     }
   }
 
-  function nextStep(){
+  function nextStep() {
 
     const nextId = idNextQuestion()
 
@@ -59,11 +61,11 @@ export default function Home() {
 
   }
 
-  function nextQuestion(nextId: number){
-      loadQuestion(nextId)
+  function nextQuestion(nextId: number) {
+    loadQuestion(nextId)
   }
 
-  function finish(){
+  function finish() {
     router.push({
       pathname: "/result",
       query: {
@@ -74,12 +76,22 @@ export default function Home() {
   }
 
 
-  return question ? (
-     <Quiz 
-        question={question}
-        lastQuestion={idNextQuestion() === undefined}
-        answeredQuestion={answeredQuestion}
-        nextStep={nextStep}
-      />  
-  ): false
+  return (
+    <div style={{
+      width: '100%',
+      height: '100vh',
+    }}>
+      {question ? (
+        <Quiz
+
+          question={question}
+          lastQuestion={idNextQuestion() === undefined}
+          answeredQuestion={answeredQuestion}
+          nextStep={nextStep}
+        />
+      ) : false
+      }
+    </div>
+
+  )
 }
